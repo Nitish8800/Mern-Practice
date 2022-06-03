acad1118c4d13812fc1cdebda598d6c8bfbdfa88
Nitish8800 <nitishfea120784@gmail.com>
2022-06-03 21:53:57 +0530

    commit

--- int.js
+++ int.js
@@ -1,11 +1,11 @@
 
-let p=[(n)=>n-1,(n)=>n*2,(n)=>n+1]
-let s=2;
-for(let i=0; i<p.length; i++)
-  {
-    s=p[i](s)
-  }
-console.log(s)
+// let p=[(n)=>n-1,(n)=>n*2,(n)=>n+1]
+// let s=2;
+// for(let i=0; i<p.length; i++)
+//   {
+//     s=p[i](s)
+//   }
+// console.log(s)
 
 
 var a=10;
@@ -15,7 +15,7 @@ var b="10"
 if(a==b)
 {    console.log(10+a+b)}
 else if(a===b){
-
+  
   console.log(20+a+b)
 
 }
